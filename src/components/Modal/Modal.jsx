855f91b450805bdd0a7c6e973172166b0ee2dc80
NichBon <bonettn@gmail.com>
2025-05-15 22:15:42 +1000

    see previous, some files were unsaved

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,11 +1,12 @@
 
 
-const Modal = ({ isOpen, onClose, bookTitle, bookAuthor, bookDescription }) => {
+const Modal = ({ isOpen, onClose, bookID, bookData}) => {
     if (!isOpen) return null;
 
     return (
         <div className="modal-overlay" onClick={onClose}>
             <div className="modal-content" onClick={(e) => e.stopPropagation()}>
+                {}
                 <h2>{bookTitle}</h2>
                 <p><strong>Author:</strong> {bookAuthor}</p>
                 <p>{bookDescription}</p>
